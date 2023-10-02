c0e9c6c861aa2f29944e4128fc98e2658e7ad2a4
Edward Gadawski <edwardgadawski@gmail.com>
2023-10-02 21:10:11 +0200

    Dodanie komponentu z przyciskiem od feedbacku

---
+++ AirApp.SharedComponents/Dialogs/FeedbackDialog.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AirApp.ComponentsShared.Dialogs;
+
+public partial class FeedbackDialog
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+
+    void Close() => MudDialog.Close(DialogResult.Ok(true));
+}
---
+++ AirApp.SharedComponents/Shared/Feedback.razor.cs
@@ -0,0 +1,19 @@
+﻿using AirApp.ComponentsShared.Dialogs;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AirApp.ComponentsShared.Shared;
+
+public partial class Feedback
+{
+    [Inject] public IDialogService DialogService { get; set; }
+
+    private void OpenDialog()
+    {
+        var options = new DialogOptions { 
+            CloseOnEscapeKey = true,
+            NoHeader = true
+        };
+        DialogService.Show<FeedbackDialog>(string.Empty, options);
+    }
+}
